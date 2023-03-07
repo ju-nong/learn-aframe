957b8973115d7c8ae2097d67e15a3e8ed507889a
이준용 <iiiuooyoo@gmail.com>
2023-03-07 16:46:53 +0900

    feat : 바닥은 plan말고 Entity로 해야지 repeat가 잘 적용됨

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Scene, Entity } from "@belivvr/aframe-react";
 
 function App() {
-    return <div>init</div>;
+    const [rendered, setRendered] = useState<boolean>(false);
+
+    useEffect(() => {
+        setRendered(true);
+
+        if (typeof window !== "undefined") {
+            require("aframe"); // eslint-disable-line global-require
+        }
+    }, [setRendered]);
+
+    if (!rendered) {
+        return <>하이요</>;
+    }
+
+    return (
+        <Scene>
+            <Entity
+                geometry={{
+                    primitive: "plane",
+                    width: 100,
+                    height: 100,
+                }}
+                rotation={{
+                    x: -90,
+                    y: 0,
+                    z: 0,
+                }}
+                material={{
+                    src: "texture/grass.jpg",
+                    repeat: { x: 100, y: 100 },
+                }}
+            />
+        </Scene>
+    );
 }
 
 export default App;
